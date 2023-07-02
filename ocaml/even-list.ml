245347c08e17aaf886aabb670383865f67bea465
Dimitri Tabatadze <tabatadzedima@protonmail.com>
2023-07-02 16:04:05 +0400

    completed solution for even-list

--- ocaml/even-list.ml
+++ ocaml/even-list.ml
@@ -1,8 +1,8 @@
 let rec el a lst = match lst with
   | h::_::t -> el (h::a) t
   | [h] -> el (h::a) []
-  | _ -> a
+  | [] -> a
 
 let rec de i a lst = match lst with
   | h::t -> de (i+1) (if i mod 2 = 0 then h::a else a) t
-  | _ -> a
+  | [] -> a
