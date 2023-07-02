d01a76cdfe13bf2f7f7c25d519a4397bab5dda3b
Dimitri Tabatadze <tabatadzedima@protonmail.com>
2023-07-02 17:31:52 +0400

    prettier

--- ocaml/-.ml
+++
@@ -1,4 +0,0 @@
-type node = Empty | Inner of node * int * node
-type path = End | Left of path | Right of path
-
-let rec tree_from_list = 
--- ocaml/bigotree.ml
+++ ocaml/bigotree.ml
@@ -1,19 +1,19 @@
 type node = Empty | Inner of node * int * node
 
-let rec insert_in_tree v t = match t with
+let rec insertintree v t = match t with
   | Empty -> Inner (Empty, v, Empty)
   | Inner (l, u, r) -> if v > u then 
-      Inner (l, u, insert_in_tree v r) 
+      Inner (l, u, insertintree v r) 
     else 
-      Inner (insert_in_tree v l, u, r)
+      Inner (insertintree v l, u, r)
 
-let rec to_tree a lst = match lst with
+let rec totree a lst = match lst with
   | [] -> a
-  | h::t -> insert_in_tree h (to_tree a t)
+  | h::t -> insertintree h (totree a t)
 
-let rec to_list t = match t with
+let rec tolist t = match t with
   | Empty -> []
-  | Inner (l, v, r) -> to_list l @ [v] @ to_list r
+  | Inner (l, v, r) -> tolist l @ [v] @ tolist r
 
 let rec insert n lst = match lst with
   | [] -> [n]
