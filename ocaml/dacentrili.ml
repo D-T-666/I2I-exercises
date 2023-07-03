e6726acd0ddedbd6b54adffa56f67c8a06e06570
Dimitri Tabatadze <tabatadzedima@protonmail.com>
2023-07-03 14:40:37 +0400

    minor updates

--- ocaml/dacentrili.ml
+++
@@ -1,9 +0,0 @@
-let rec damcentravi a lst = match lst with
-  | [] -> a
-  | h::t -> damcentravi (h::(List.rev a)) t
-  
-let rec get_nth n lst = match lst with
-  | [] -> 0
-  | h::t -> if n = 0 then h else get_nth (n-1) t
-
-let pick_middle lst = get_nth ((List.length lst) / 2) lst;;
